a387608b314f5c9ce8a5bdbf52591f6cab7c8206
kangyu <564683645@qq.com>
2026-05-05 16:30:32 +0800

    chore: initialize spring boot project

---
+++ src/main/java/com/example/superpowers/SuperpowersSpringbootLabApplication.java
@@ -0,0 +1,12 @@
+package com.example.superpowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperpowersSpringbootLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperpowersSpringbootLabApplication.class, args);
+    }
+}
